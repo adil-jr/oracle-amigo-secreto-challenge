eeb3c9a11377b6376ffd5a51071a40e2acd5fc50
adil-jr <adil.jr@outlook.com>
2025-02-15 10:33:22 -0300

    Atualizar lista

--- app.js
+++ app.js
@@ -18,3 +18,29 @@ function adicionarAmigo() {
     atualizarLista();
     input.value = "";
 }
+
+function atualizarLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    
+    listaAmigos.forEach((amigo, index) => {
+        let li = document.createElement("li");
+        li.style.display = "flex";
+        li.style.justifyContent = "space-between";
+        li.style.alignItems = "center";
+        li.style.padding = "5px";
+        li.style.gap = "10px";
+        
+        let spanNome = document.createElement("span");
+        spanNome.textContent = amigo;
+        
+        let botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "Remover";
+        botaoRemover.style.marginLeft = "10px";
+        botaoRemover.onclick = () => removerAmigo(index);
+        
+        li.appendChild(spanNome);
+        li.appendChild(botaoRemover);
+        ul.appendChild(li);
+    });
+}
